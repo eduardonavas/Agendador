4320ae908b7f5cedd8fd56368b03d9ba30f80336
eduardonavas <eduardo_rnavas@yahoo.com.br>
2017-09-17 21:54:06 -0300

    incluida a chamada da operacaoD

--- src/main/java/br/com/cvc/controller/CadastrarAgendamento.java
+++ src/main/java/br/com/cvc/controller/CadastrarAgendamento.java
@@ -1,5 +1,8 @@
 package br.com.cvc.controller;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -8,21 +11,31 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import br.com.cvc.db.Agendamentos;
+import br.com.cvc.exceptions.ExceptionRegraDeNegocio;
 import br.com.cvc.model.Agendamento;
+import br.com.cvc.operacoes.OperacaoD;
 
 
 @Controller
 public class CadastrarAgendamento {
 
+	private final String DATE_PATTERN = "yyyy-MM-dd";
+	
+	
 	@RequestMapping(value = "/cadastrarAgendamento", method = RequestMethod.POST)
-	public ResponseEntity<Agendamento> cadastrar(@RequestBody Agendamento agendamento){
+	public ResponseEntity<Agendamento> cadastrar(@RequestBody Agendamento agendamento) throws ExceptionRegraDeNegocio{
+		
+		
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern(DATE_PATTERN);
+		agendamento.setDataAgendamento(LocalDate.now().format(formatter));
+		
+		new OperacaoD().operacaoD(agendamento);
+		
 		Agendamentos.getInstance().add(agendamento);
-		try{
-			return new ResponseEntity<>(HttpStatus.OK);
-		}catch (Exception e) {
-			// TODO: handle exception
-			return new ResponseEntity<>(HttpStatus.EXPECTATION_FAILED);
-		}
+		
+		
+		return new ResponseEntity<>(HttpStatus.OK);
+		
 		
 	}
 }
